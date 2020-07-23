19670bcb379242f29afd82854bf5fb66cc6cf243
littleDrive <ZERO.OU@OOCL.COM>
2020-07-23 14:45:19 +0800

    [FizzBuzzWhizzTest] [zero_ou] refactor: update playFizzBuzz

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -6,46 +6,20 @@ public class FizzBuzz {
     public static String playFizzBuzz(int number) {
 
         String result = "";
-        if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0) {
-            return "FizzBuzzWhizz";
-        }
-
-        if (number % 3 == 0 && number % 5 == 0) {
-            return "FizzBuzz";
-        }
-        if (number % 3 == 0 && number % 7 == 0) {
-            return "FizzWhizz";
-        }
-        if (number % 5 == 0 && number % 7 == 0) {
-            return "BuzzWhizz";
-        }
 
         if (number % 3 == 0) {
-            return   "Fizz";
+            result +=  "Fizz";
         }
         if (number % 5 == 0) {
-            return  "Buzz";
+            result += "Buzz";
         }
         if (number % 7 == 0) {
-            return  "Whizz";
+            result += "Whizz";
         }
 
-        return String.valueOf(number);
-
-
-//        if (number % 3 == 0) {
-//            result +=  "Fizz";
-//        }
-//        if (number % 5 == 0) {
-//            result += "Buzz";
-//        }
-//        if (number % 7 == 0) {
-//            result += "Whizz";
-//        }
-//
-//        if (result.equals("")) {
-//            result = String.valueOf(number);
-//        }
-//        return result;
+        if (result.equals("")) {
+            result = String.valueOf(number);
+        }
+        return result;
     }
 }
