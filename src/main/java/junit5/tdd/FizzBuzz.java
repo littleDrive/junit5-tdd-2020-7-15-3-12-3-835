3bc835c893312c644a2a6e29abe61606be2f101e
littleDrive <ZERO.OU@OOCL.COM>
2020-07-23 13:18:45 +0800

    [FizzBuzzTest] [zero_ou] Feature: implement should_return_Whizz_when_playFizzBuzz_given_7

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -9,11 +9,15 @@ public class FizzBuzz {
 //        if (number % 3 == 0 && number % 5 == 0) {
 //            return "FizzBuzz";
 //        }
+
         if (number % 3 == 0) {
             result +=  "Fizz";
         }
         if (number % 5 == 0) {
             result += "Buzz";
+        }
+        if (number % 7 == 0) {
+            result += "Whizz";
         }
         if (result.equals("")) {
             result = String.valueOf(number);
