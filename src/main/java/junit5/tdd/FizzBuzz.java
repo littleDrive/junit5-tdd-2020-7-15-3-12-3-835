f1ef5c586b4b10583f915941f405de7353df4d27
littleDrive <ZERO.OU@OOCL.COM>
2020-07-23 11:47:36 +0800

    [FizzBuzzTest] [zero_ou] Feature: implement should_return_Fizz_when_playFizzBuzz_given_3 in palyFizzBuzz

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -3,6 +3,9 @@ package junit5.tdd;
 public class FizzBuzz {
 
     public static String playFizzBuzz(int number) {
+        if (number == 3) {
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
